07156f9d3a3fe1d70a321618c773cc9829c2ae12
jarison102 <110105324+jarison102@users.noreply.github.com>
2022-10-17 15:30:38 -0500

    servidor

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const res = require('express/lib/response')
 const { dirname } = require('path')
 const app = express()
-const port = 3000 
+const port = process.env.PORT||3000;
 
 const path = require('path')
 app.use('/static', express.static(path.join(__dirname, 'public')))
